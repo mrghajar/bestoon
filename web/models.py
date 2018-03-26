4d93c3a0bd9062f7ce2c6b1884791a3b0bcfeb6a
GHAJAR <mrghajar@gmail.com>
2018-03-26 17:55:38 +0430

    income in models and some beatification

--- web/models.py
+++ web/models.py
@@ -5,8 +5,19 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class Expence(models.Model):
+class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.date,self.amount)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.date,self.amount)
